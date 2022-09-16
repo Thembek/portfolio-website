803230cb9ed24bf74905287d7ff74422e92e60a5
Andile <andile.goba@younglings.africa>
2022-09-16 11:02:25 +0200

    scrollup button commit

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -3,10 +3,19 @@ import React from 'react';
 import './scrollup.css';
 
 const Scrollup = () => {
+    window.addEventListener('scroll', function () {
+        const scrollUp = document.querySelector('.scrollup');
+        if (this.scollY >= 560){
+            scrollUp.classList.add("show-scroll");
+        } else {
+            scrollUp.classList.remove("show-scroll");
+        }
+        
+    });
     return(
-        <>
-            Scrollup
-        </>
+        <a href="#home" className="scrollup">
+            <i className="uil uil-arrow-up scrollup__icon"></i>
+        </a>
     );
 }
 
