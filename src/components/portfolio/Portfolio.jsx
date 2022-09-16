61f38c874143cf6e60af3c572fc25efdd271866a
Andile <andile.goba@younglings.africa>
2022-09-16 15:21:43 +0200

    portfolio folder update commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
+import Work1 from '../../assets/work1.jpg';
+import Work2 from '../../assets/work2.jpg';
+import Work3 from '../../assets/work3.jpg';
+import Work4 from '../../assets/work4.jpg';
 
 import './portfolio.css';
 
 const Portfolio = () => {
     return (
-        <div className="portfoliio" id="portfolio">
+        <div className="portfolio" id="portfolio">
             <h2 className="section__title">My Portfolio</h2>
             <span className="section__subtitle">Built projects</span>
 
             <Swiper
-                spaceBetween={25}
-                slidesPreview={3}
-                className="portfolio__slider"
+                spaceBetween={30}
+                slidesPerView={3}
+                grabCursor={true}
+                className="portfolio-slider"
             >
                 <SwiperSlide>
-                    <img src={} alt="" />
-                </SwiperSlide>
-
-            </Swiper>
+                <img src={Work1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Work2} alt="" />
+            </SwiperSlide>
+             <SwiperSlide>
+                <img src={Work3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Work4} alt="" />
+            </SwiperSlide>
+        </Swiper>
         </div>
     );
 }
